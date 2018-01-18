a9637b4f1eeb319bff99241b5295b14cac75471f
Oliver Sanderson <osanderson@wpi.edu>
2018-01-18 01:38:31 -0500

    Started search page

---
+++ public/js/search.js
@@ -0,0 +1,18 @@
+(function() {
+    var config = {
+        apiKey: "AIzaSyCFWzxl0VLYePJ-5O8U5umWWNJLT7TG9Fo",
+        authDomain: "urmatt-app.firebaseapp.com",
+        databaseURL: "https://urmatt-app.firebaseio.com",
+        projectId: "urmatt-app",
+        storageBucket: "urmatt-app.appspot.com",
+        messagingSenderId: "523826665141"
+    };
+    firebase.initializeApp(config);
+
+}());
+
+var ref = firebase.database().ref("reports");
+
+ref.orderByValue().on("child_added", function(snapshot){
+    console.log(snapshot.key);
+});
