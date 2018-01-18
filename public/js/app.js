04fcf1af79b3fc107d16330dcb1685fdfac39551
Oliver Sanderson <osanderson@wpi.edu>
2018-01-18 06:31:17 -0500

    Lots of packages added and removed, switched over to firestore from rtdb

--- public/js/app.js
+++ public/js/app.js
@@ -1,52 +1,36 @@
-//      var button = document.querySelector("button");
-// Initialize Firebase
-(function() {
-    var config = {
-        apiKey: "AIzaSyCFWzxl0VLYePJ-5O8U5umWWNJLT7TG9Fo",
-        authDomain: "urmatt-app.firebaseapp.com",
-        databaseURL: "https://urmatt-app.firebaseio.com",
-        projectId: "urmatt-app",
-        storageBucket: "urmatt-app.appspot.com",
-        messagingSenderId: "523826665141"
-    };
+
+// const firebase = require("firebase");
+// // Required for side-effects
+// require("firebase/firestore");
+
+var config = {
+    apiKey: "AIzaSyCFWzxl0VLYePJ-5O8U5umWWNJLT7TG9Fo",
+    authDomain: "urmatt-app.firebaseapp.com",
+    databaseURL: "https://urmatt-app.firebaseio.com",
+    projectId: "urmatt-app",
+    storageBucket: "urmatt-app.appspot.com",
+    messagingSenderId: "523826665141"
+};
+// if(!firebase.apps.length){
     firebase.initializeApp(config);
-
-//     const preObject = document.querySelector("#object");
-//     const ulList = document.querySelector("#list");
-//
-//     const dbRefObject = firebase.database().ref().child('object');
-//     const dbRefList = dbRefObject.child('hobbies');
-//
-//
-//     dbRefObject.on("value", snap => {
-//         preObject.innerText = JSON.stringify(snap.val(), null, 3);
-//     });
-//     dbRefList.on('child_added', snap => {
-//         const li = document.createElement('li');
-//         li.innerText = snap.val();
-//         li.id = snap.key;
-//         ulList.appendChild(li);
-//     });
-//
-//     dbRefList.on('child_changed', snap => {
-//         const liChanged = document.getElementById(snap.key);
-//         liChanged.innerText = snap.val();
-//     });
-//
-//     dbRefList.on('child_removed', snap => {
-//         const liRemoved = document.getElementById(snap.key);
-//     liRemoved.remove();
-// });
-//     var database = firebase.database();
-//     buttonData(database);
-}());
-
-
-
-// function buttonData(db) {
-//     button.addEventListener("click", function () {
-//         db.ref('object').set({
-//             username: "testuser2"
-//         });
-//     });
 // }
+var db = firebase.firestore();
+
+db.collection("reports").add({
+    eggs: 8,
+    firstName: "Joe",
+    lastName: "Schmoe"
+});
+
+// fetch('https://urmatt-app.firebaseio.com/reports.json')
+// fetch('https://firestore.googleapis.com/projects/urmatt-app/databases/reports/documents')
+//     .then(function(res){
+//         console.log(res);
+//         return res.json();
+//     })
+//     .then(function(data){
+//         console.log(Object.entries(JSON.parse(JSON.stringify(data))));
+//     })
+//     .catch(function(err){
+//         console.log(err);
+//     });
--- public/js/db.js
+++ public/js/db.js
@@ -1,42 +1,48 @@
-var submitBtn = document.querySelector("#submitBtn");
-var form = document.querySelector("form");
-var eggInput = document.querySelector("#eggInput");
-var firstNameInput = document.querySelector("#firstName");
-var lastNameInput = document.querySelector("#lastName");
+// var submitBtn = document.querySelector("#submitBtn");
+// var form = document.querySelector("form");
+// var eggInput = document.querySelector("#eggInput");
+// var firstNameInput = document.querySelector("#firstName");
+// var lastNameInput = document.querySelector("#lastName");
+//
+// var config = {
+//     apiKey: "AIzaSyCFWzxl0VLYePJ-5O8U5umWWNJLT7TG9Fo",
+//     authDomain: "urmatt-app.firebaseapp.com",
+//     // databaseURL: "https://urmatt-app.firebaseio.com",
+//     projectId: "urmatt-app",
+//     // storageBucket: "urmatt-app.appspot.com",
+//     // messagingSenderId: "523826665141"
+// };
+// firebase.initializeApp(config);
+//
+//
+// var db = firebase.firestore();
+//
+// db.collection("reports").add({
+//     eggs: 7,
+//     firstName: "Joe",
+//     lastName: "Schmoe"
+// });
 
-(function() {
-    var config = {
-        apiKey: "AIzaSyCFWzxl0VLYePJ-5O8U5umWWNJLT7TG9Fo",
-        authDomain: "urmatt-app.firebaseapp.com",
-        databaseURL: "https://urmatt-app.firebaseio.com",
-        projectId: "urmatt-app",
-        storageBucket: "urmatt-app.appspot.com",
-        messagingSenderId: "523826665141"
-    };
-    firebase.initializeApp(config);
-
-}());
-
-function submitReport(data){
-
-    // Get a key for a new User
-    var newReportKey = firebase.database().ref().child('reports').push().key;
-
-    var updates = {};
-    updates['/reports/' + newReportKey] = data;
-
-    return firebase.database().ref().update(updates);
-}
-
-submitBtn.addEventListener("click", function(e) {
-    e.preventDefault();
-    var data = {
-        firstName: firstNameInput.value,
-        lastName: lastNameInput.value,
-        eggs: eggInput.value,
-        timestamp: Date.now()
-    };
-    submitReport(data);
-    form.reset();
-});
+// function submitReport(data){
+//
+//     // Get a key for a new User
+//     var newReportKey = firebase.database().ref().child('reports').push().key;
+//
+//     var updates = {};
+//     updates['/reports/' + newReportKey] = data;
+//
+//     return firebase.database().ref().update(updates);
+// }
+//
+// submitBtn.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     var data = {
+//         firstName: firstNameInput.value,
+//         lastName: lastNameInput.value,
+//         eggs: eggInput.value,
+//         timestamp: Date.now()
+//     };
+//     submitReport(data);
+//     form.reset();
+// });
 
